488e3398c6c6f40927f570ddd83a8f37129a58d2
heitorAlbu <heitor.albuquerque@gmail.com>
2023-04-09 16:08:21 -0300

    implementando importação de font de forma global

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Estilos } from "./componentes/EstilosGlobais/Estilos"
 
 function App() {
   return (
     <div>
+     <Estilos/>
       <h1>Freelando</h1>
     </div>
   );
