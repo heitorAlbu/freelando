8cd1b7c22d10c022594b636498bd18a4397113df
Heitor <heitor.albuquerque@ferreiracosta.com.br>
2023-05-07 11:45:48 -0300

    adicionando Tipografia + label e input estilizados

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 
 import { Estilos } from "./componentes/EstilosGlobais/Estilos"
-import {Card} from "./componentes/Card/Card"
+import { Card } from "./componentes/Card/Card"
 import { ProvedorTema } from "./componentes/ProvedorTema/ProvedorTema";
+import { Tipografia } from "./componentes/Tipografia/Tipografia";
+import { CampoTexto } from "./componentes/CampoTexto/CampoTexto";
 function App() {
   return (
     <ProvedorTema>
-     <Estilos/>
-     <Card>
-     <h1>Freelando</h1>
-     </Card>
+      <Estilos />
+      <Card>
+        <Tipografia variante="h1" componente="h1">
+          <h1>Freelando</h1>
+        </Tipografia>
+        <Tipografia variante="body" componente="body">
+          Crie seu perfil gratuitamente para começar a trabalharcom os melhores freelancers.
+          Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
+        </Tipografia>
+        <CampoTexto titulo="Nome Completo"></CampoTexto>
+      </Card>
     </ProvedorTema>
   );
 }
--- src/componentes/ProvedorTema/ProvedorTema.jsx
+++ src/componentes/ProvedorTema/ProvedorTema.jsx
@@ -1,27 +1,31 @@
-import {ThemeProvider} from '@emotion/react'
+import { ThemeProvider } from '@emotion/react'
 
 const tema = {
-    cores:{
-        primarias :{
-            a:'#5754ED',
-            b:'',
-            c:''
+    cores: {
+        primarias: {
+            a: '#5754ED',
+            b: '',
+            c: ''
         },
         secundarias: {
-            a:'#EBEAF9',
-            b:'',
-            c:''
+            a: '#EBEAF9',
+            b: '',
+            c: ''
+        },
+        neutras: {
+            a: '#373737'
         },
     },
-    espacamentos:{
-        s:'16px',
-        l:'32px'
+    espacamentos: {
+        xs: '8px',
+        s: '16px',
+        l: '32px'
 
-    }   
+    }
 
 }
 
-export const ProvedorTema = ({children}) =>{
+export const ProvedorTema = ({ children }) => {
     return <ThemeProvider theme={tema}>
         {children}
     </ThemeProvider>
